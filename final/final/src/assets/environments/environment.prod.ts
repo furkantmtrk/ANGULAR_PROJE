83b0a96de5509c635603a73deead65a1cecd4bdf
furkantmtrk <furkantmtrk>
2021-05-10 23:00:25 +0300

    1

---
+++ final/final/src/assets/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCsPRD74xHjC7n7Sq-ixQix67y7MesM2Xw",
+  authDomain: "sistem-d5b3d.firebaseapp.com",
+  projectId: "sistem-d5b3d",
+  storageBucket: "sistem-d5b3d.appspot.com",
+  messagingSenderId: "234076861565",
+  appId: "1:234076861565:web:4f503f3fdb423744642d14"
+  }
+  };
